0d1062fe0049341afa2a1307ef2a943f841e61cc
drbig30 <113547431+drbig30@users.noreply.github.com>
2023-08-09 19:34:23 +0000

    Exercise done

--- src/app.js
+++ src/app.js
@@ -7,5 +7,49 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
+  let randomNumber = Math.random() * 10;
+  console.log("Hello!" + randomNumber);
+};
+let generateExcuse = () => {
+  let pronoun = ["A ", "The "];
+  let subject = [
+    "Jogger ",
+    "Racoon ",
+    "dog ",
+    "driver ",
+    "comedian ",
+    "pincone "
+  ];
+  let actions = [
+    " took my ",
+    " threw my ",
+    " yelled at my ",
+    " stole my ",
+    " bit my "
+  ];
+  let possesion = [" homework ", " toe ", " car ", " shoe "];
+  let where = [" on the street ", " in my house ", " in my drive way "];
+
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let actionsIndex = Math.floor(Math.random() * actions.length);
+  let possesionIndex = Math.floor(Math.random() * possesion.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[pronounIndex] +
+    "" +
+    subject[subjectIndex] +
+    "" +
+    actions[actionsIndex] +
+    "" +
+    possesion[possesionIndex] +
+    "" +
+    where[whereIndex] +
+    ""
+  );
 };
